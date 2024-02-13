bb413afa4851b15ce6facd9fe495d5219e58c1ed
Gleb Struchalin <glebx-f@mail.ru>
2024-02-13 19:01:11 +0300

    Removed obsolete files

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export NODE_ENV=production
-export NEXT_TELEMETRY_DISABLED=1
-export DOCS_DIR=../../../docs/docs
-
-node app/packages/preview/start.js &
-
-cd search
-uvicorn server:app --reload
