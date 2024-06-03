19b74101f533beb59bdb7b85bee568e8c2dcdb76
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2024-06-03 19:44:52 +0700

    Updated app to version 0.1.568, updated docs

--- app/packages/preview/.next/server/server-reference-manifest.js
+++ app/packages/preview/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"+PC8Oop1L0lI8hlSFxh3++jbZ2UxQohLXIAwWVFeaB4=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"h891/8YCFleDOXOTcex/G9a5BQGjtH8Rly9NlGvzW0s=\"}"
