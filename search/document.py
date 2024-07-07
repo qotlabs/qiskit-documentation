10f7d1da1c2a01c9679b7f3263c4f468405fb915
Gleb Struchalin <glebx-f@mail.ru>
2024-07-07 17:55:25 +0300

    Updated search backend to support guides in docs

--- search/document.py
+++ search/document.py
@@ -36,11 +36,7 @@ class DocSection(Enum):
     QISKIT_IBM_PROVIDER = 3
     QISKIT_IBM_RUNTIME = 4
     QISKIT_TRANSPILER_SERVICE = 5
-    BUILD = 10
-    RUN = 20
-    START = 30
-    TRANSPILE = 40
-    VERIFY = 50
+    GUIDES = 10
 
     @staticmethod
     def from_str(s: str):
