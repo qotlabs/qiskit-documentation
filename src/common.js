89b8fc47a59793ee2138dec81087ffd0aa845fe5
Gleb Struchalin <glebx-f@mail.ru>
2024-12-28 16:11:13 +0300

    Removed "robots none" meta tag, modified page title template

--- src/common.js
+++ src/common.js
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-FileCopyrightText: Copyright (c) 2024 Quantum Optical Technologies Laboratories
 // SPDX-FileContributor: Gleb Struchalin <struchalin.gleb@physics.msu.ru>
+// SPDX-FileContributor: Fedor Medvedev <fedor_medvedev42@rambler.ru>
 
 /**
  * Top-level menu structure.
