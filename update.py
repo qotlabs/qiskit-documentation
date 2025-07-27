f4bb95266e27156413c59f492bd8887c1f4eb394
Gleb Struchalin <glebx-f@mail.ru>
2025-07-27 21:05:29 +0700

    Modified update script

--- update.py
+++ update.py
@@ -3,10 +3,19 @@
 import os
 import tarfile
 import json
+import argparse
+import shutil
+import errno
+from pathlib import Path
 
-def get_members(tar: tarfile.TarFile, path: str,
-                node_modules: bool = True,
-                verbose: bool = False):
+
+def path_relative_to_script(path: Path | str) -> Path:
+    return (Path(__file__).parent / path).relative_to(Path.cwd())
+
+
+def get_tar_members(tar: tarfile.TarFile, path: str,
+                    node_modules: bool = True,
+                    verbose: bool = False):
     if not path.endswith("/"):
         path += "/"
     offset = len(path)
@@ -19,9 +28,14 @@ def get_members(tar: tarfile.TarFile, path: str,
                 print(info.name)
             yield info
 
-if __name__ == '__main__':
-    os.system("./docker_pull.py icr.io/qc-open-source-docs-public/preview@latest")
-    with tarfile.open("qc-open-source-docs-public_preview.tar", "r") as image_tar:
+
+def do_download(args):
+    docker_pull = path_relative_to_script("docker_pull.py")
+    os.system(f"./{docker_pull} icr.io/qc-open-source-docs-public/preview@{args.digest}")
+
+
+def do_extract(args):
+    with tarfile.open(args.tar, "r") as image_tar:
         with image_tar.extractfile("manifest.json") as manifest_file:
             manifest = json.load(manifest_file)[0]
             layer_paths = manifest["Layers"]
@@ -29,5 +43,53 @@ if __name__ == '__main__':
             print(f"Extracting layer {layer_path}")
             with image_tar.extractfile(layer_path) as layer_file:
                 with tarfile.open(fileobj=layer_file) as layer_tar:
-                    members = get_members(layer_tar, "home/node/app")
-                    layer_tar.extractall("app_new", members, filter="data")
+                    members = get_tar_members(layer_tar, "home/node/app")
+                    layer_tar.extractall(args.dir, members, filter="data")
+
+
+def do_cache_clean(args):
+    cache_dir = path_relative_to_script("app/packages/preview/.next/cache/")
+    if cache_dir.exists():
+        shutil.rmtree(cache_dir)
+    print(f"Cleaned {cache_dir}")
+
+    EXTENSIONS = ".body", ".html", ".meta", ".out", ".rsc"
+    app_dir = path_relative_to_script("app/packages/preview/.next/server/app/")
+    for root, dirs, files in app_dir.walk(top_down=False):
+        for name in files:
+            if name.endswith(EXTENSIONS):
+                (root / name).unlink()
+        for name in dirs:
+            try:
+                (root / name).rmdir()
+            except OSError as e:
+                if e.errno != errno.ENOTEMPTY:
+                    raise
+    print(f"Cleaned {app_dir}/*{{{','.join(EXTENSIONS)}}}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Update Qiskit documentation application")
+    subparsers = parser.add_subparsers(dest="command", title="commands",
+                                       required=True)
+    # Download
+    download = subparsers.add_parser("download", help="Download Docker image")
+    download.set_defaults(func=do_download)
+    download.add_argument("--digest", "-d",
+                          help="Docker image digest for downloading",
+                          default="latest")
+    # Extract
+    extract = subparsers.add_parser("extract", help="Extract Docker image from TAR")
+    extract.set_defaults(func=do_extract)
+    extract.add_argument("--tar", "-t", help="Image TAR filename",
+                         metavar="<filename>", default="qc-open-source-docs-public_preview.tar")
+    extract.add_argument("--dir", "-d", help="Output directory name",
+                         metavar="<dirname>", default="app_new")
+    # Cache clean
+    cache_clean = subparsers.add_parser("cache-clean", aliases=["cc"],
+                                        help="Clean app cache")
+    cache_clean.set_defaults(func=do_cache_clean)
+
+    args = parser.parse_args()
+    args.func(args)
