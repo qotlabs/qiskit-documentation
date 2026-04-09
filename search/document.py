4159b65771ea49029d69bf84aec6c539571658e7
Gleb Struchalin <glebx-f@mail.ru>
2026-04-09 17:16:22 +0300

    Updated docs

--- search/document.py
+++ search/document.py
@@ -90,6 +90,7 @@ class DocSection:
     311: ("/learning/courses/variational-algorithm-design/", "Courses / Variational algorithm design"),
     312: ("/learning/courses/integrating-quantum-and-high-performance-computing/", "Cources / Integrating quantum and high-performance computing"),
     313: ("/learning/courses/designing-and-leading-quantum-projects/", "Courses / Designing and leading quantum projects"),
+    314: ("/learning/courses/use-a-qc-today/", "Cources / Use a quantum computer today"),
     320: ("/learning/modules/computer-science/", "Modules / Computer science"),
     321: ("/learning/modules/quantum-mechanics/", "Modules / Quantum mechanics"),
     }
