37fcd576bb1b888242dc5d0dc1cd97824df419dc
Gleb Struchalin <glebx-f@mail.ru>
2026-09-01 17:50:17 +0300

    Updated docs and added missing sections

--- src/make_robots.js
+++ src/make_robots.js
@@ -9,32 +9,31 @@ Allow: /
 Host: ${HOST}
 Sitemap: ${HOST}/sitemap.xml
 
+Disallow: /docs/api/pauli-prop/0.*
+
 Disallow: /docs/api/qiskit/0.*
 Disallow: /docs/api/qiskit/1.*
 Disallow: /docs/api/qiskit/2.*
 Disallow: /docs/api/qiskit/dev
 
+Disallow: /docs/api/qiskit-addon-aqc-tensor/0.*
 Disallow: /docs/api/qiskit-addon-cutting/0.*
-
-Disallow: /docs/api/qiskit-ibm-runtime/0.*
-Disallow: /docs/api/qiskit-ibm-runtime/dev
+Disallow: /docs/api/qiskit-addon-mpf/0.*
+Disallow: /docs/api/qiskit-addon-obp/0.*
+Disallow: /docs/api/qiskit-addon-paulice/0.*
+Disallow: /docs/api/qiskit-addon-pna/0.*
+Disallow: /docs/api/qiskit-addon-slc/0.*
+Disallow: /docs/api/qiskit-addon-sqd/0.*
+Disallow: /docs/api/qiskit-addon-utils/0.*
 
 Disallow: /docs/api/qiskit-c/2.*
 Disallow: /docs/api/qiskit-c/dev
 
-Disallow: /docs/api/qiskit-addon-aqc/0.*
+Disallow: /docs/api/qiskit-fermions/0.*
+Disallow: /docs/api/qiskit-fermions-c/0.*
 
-Disallow: /docs/api/qiskit-addon-cutting/0.*
-
-Disallow: /docs/api/qiskit-addon-mpf/0.*
-
-Disallow: /docs/api/qiskit-addon-obp/0.*
-
-Disallow: /docs/api/qiskit-addon-paulice/0.*
-
-Disallow: /docs/api/qiskit-addon-sqd/0.*
-
-Disallow: /docs/api/qiskit-addon-utils/0.*
+Disallow: /docs/api/qiskit-ibm-runtime/0.*
+Disallow: /docs/api/qiskit-ibm-runtime/dev
 
 Disallow: /docs/api/qiskit-ibm-transpiler/0.*
 `;
